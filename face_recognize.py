4596a42d21be1a248b124571fbb2b120272d5f66
ADM <darkgaroto@gmail.com>
2021-06-10 17:25:44 -0300

    :white_check_mark: Atualização de parâmetros

--- face_recognize.py
+++ face_recognize.py
@@ -19,10 +19,11 @@ if not os.path.isfile(fname):
   exit(0)
 
 face_cascade = cv2.CascadeClassifier('Haar/haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier('Haar/haarcascade_eye.xml')
 cap = cv2.VideoCapture(1)
 
 #cria um objeto CLAHE
-clahe = cv2.createCLAHE(clipLimit=2.5, tileGridSize=(8,8))
+#clahe = cv2.createCLAHE(clipLimit=2.1, tileGridSize=(8,8))
 
 recognizer = cv2.face.LBPHFaceRecognizer_create()
 recognizer.read(fname)
@@ -31,25 +32,30 @@ while True:
 
   _,img = cap.read()
   gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-  equalize_image = clahe.apply(gray)
-  faces = face_cascade.detectMultiScale(equalize_image, 1.1, 4)
+  faces = face_cascade.detectMultiScale(gray, 1.1, 5)
 
   for (x,y,w,h) in faces:
-    cv2.rectangle(img,(x,y),(x+w,y+h),(255,255,255),2)
-    ids,conf = recognizer.predict(gray[y:y+h,x:x+w])
-    c.execute("select name from users where id = (?);", (ids,))
-    result = c.fetchall()
-    name = result[0][0]
 
-    if conf < 50:
-      cv2.putText(img, name, (x+2,y+h-5), cv2.FONT_HERSHEY_SIMPLEX, 1, (150,255,0),2)
-      cv2.putText(img, str(conf), (x,y + (h+30)), cv2.FONT_HERSHEY_SIMPLEX, 1, (150,255,0), 2)
-      print('[SISTEMA] Seja bem-vindo ' + str(name))
-      #GPIO.output(18, GPIO.HIGH)
+    gray_face = gray[y: y+h, x: x+w]
+    eyes = eye_cascade.detectMultiScale(gray_face)
+
+    for(ex, ey, ew, eh) in eyes:
+      cv2.rectangle(img,(x,y),(x+w,y+h),(255,255,255),2)
+
+      ids,conf = recognizer.predict(gray[y:y+h,x:x+w])
+      c.execute("select name from users where id = (?);", (ids,))
+      result = c.fetchall()
+      name = result[0][0]
+
+      if conf < 50:
+        cv2.putText(img, name, (x+2,y+h-5), cv2.FONT_HERSHEY_SIMPLEX, 1, (150,255,0),2)
+        cv2.putText(img, str(conf), (x,y + (h+30)), cv2.FONT_HERSHEY_SIMPLEX, 1, (150,255,0), 2)
+        print('[SISTEMA] Seja bem-vindo ' + str(name))
+        #GPIO.output(18, GPIO.HIGH)
       
-    else:
-      cv2.putText(img, 'Desconhecido', (x+2,y+h-5), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255),2)
-      #GPIO.output(18, GPIO.LOW)
+      else:
+        cv2.putText(img, 'Desconhecido', (x+2,y+h-5), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255),2)
+        #GPIO.output(18, GPIO.LOW)
 
   cv2.imshow('Reconhecimento Facial',img)
    
